47b5501f0442b36d361a32de886cd1e9f68b49b5
realcdugz <connorduggan30@gmail.com>
2024-12-04 14:42:25 -0600

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-// import axios from "axios";
-// import { Header } from "./Header";
-// import { TodoPage } from "./TodoPage";
-// import { Footer } from "./Footer";
-// import { SignupPage } from "./SignupPage";
-// import { LoginPage } from "./LoginPage";
-// import { LogoutLink } from "./LogoutLink";
-
-// axios.defaults.baseURL = "http://localhost:3000";
-// axios.defaults.withCredentials = true;
-
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       <SignupPage />
-//       <LoginPage />
-//       <LogoutLink />
-//       <TodoPage />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import axios from "axios";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { SignupPage } from "./SignupPage";
 import { LoginPage } from "./LoginPage";
-import { LogoutLink } from "./LogoutLink";
 import { TodoPage } from "./TodoPage";
 import { Footer } from "./Footer";
 
