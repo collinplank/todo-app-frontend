acc56609047de533fcf8ab146660081ff8d00d6f
realcdugz <connorduggan30@gmail.com>
2024-12-05 12:45:47 -0600

    Added footer styling and made header darker

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="bg-slate-800 shadow-md fixed w-full top-0 z-50">
+    <header className="bg-slate-950 shadow-md fixed w-full top-0 z-50">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-4">
